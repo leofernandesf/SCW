037b02721af2f889c8ed1b3e72ce8165b515c543
Leonardo Fernandes Farias <leonardo.fernandesf@gmail.com>
2016-11-08 10:53:57 -0400

    Initial Commit

---
+++ SCW-MUSASHI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCW-MUSASHI
+//
+//  Created by leo on 08/11/16.
+//  Copyright © 2016 tap4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
